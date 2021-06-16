9ed5b42421321d5a00209389b798683286686eba
khutjo <kmaputla@student.wethinkcode.co.za>
2021-06-16 17:59:47 +0200

    add passport

--- index.js
+++ index.js
@@ -2,13 +2,47 @@ const express = require('express');
 let app = express();
 let port = process.env.PORT || 3000;
 
+var GitHubStrategy = require('passport-github').Strategy;
 
 
-app.get('/', (req, res) => {
+var passport = require('passport');
 
-    res.send("fgfgfg");
-});
+const DatabaseCreds = {
+    host: 'remotemysql.com',
+    user: 'CsJRuhA9v6',
+    password: 'G0rXiQLh7c',
+    database: 'CsJRuhA9v6',
+    port: 3306
+}
 
+
+
+
+
+passport.use(new GitHubStrategy({
+        clientID: '1bee8f2166a6f101a6aa',
+        clientSecret: '62f3a23da5dc8c1740dcade29c6d41ea2bf3a343',
+        callbackURL: "http://localhost:3000/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        console.log(accessToken);
+        console.log(refreshToken);
+        console.log(profile);
+        return cb('im done');
+    }
+));
+
+
+
+app.get('/auth/error', (req, res) => res.send('Unknown Error'))
+
+app.get('/', passport.authenticate('github', { scope: ['user:email'] }));
+
+app.get('/callback',
+    passport.authenticate('github', { failureRedirect: '/auth/error' }),
+    function(req, res) {
+        res.redirect('/callback');
+    });
 
 
 
