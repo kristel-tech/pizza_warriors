5646615cddc074ac3b583f83f714695d75474d5c
khutjo <kmaputla@student.wethinkcode.co.za>
2021-06-18 00:36:59 +0200

    add db funcs

---
+++ DBConnection/config.js
@@ -0,0 +1,11 @@
+const DatabaseCreds = {
+    host: 'remotemysql.com',
+    user: 'CsJRuhA9v6',
+    password: 'G0rXiQLh7c',
+    database: 'CsJRuhA9v6',
+    port: 3306
+
+}
+
+
+module.exports = DatabaseCreds
